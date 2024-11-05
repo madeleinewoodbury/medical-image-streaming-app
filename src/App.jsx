d5333ec7ed708552f85b0317018836594a659e54
Madeleine Woodbury <mail@woodburycode.com>
2024-11-04 19:41:41 -0500

    inintal commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WebRTCImageStream from './pages/WebRTCStream'
+
+function App() {
+	return (
+		<main className='container mx-auto'>
+			<WebRTCImageStream />
+		</main>
+	)
+}
+
+export default App
